c2921eb8f9e2ec4bcf3513497673c7bdd0b44773
Davut <2005davut55@gmail.com>
2024-11-29 23:37:33 +0200

    Improving the Profile Smart Contract

--- contracts/Twitter-Profile.sol
+++ contracts/Twitter-Profile.sol
@@ -5,12 +5,13 @@ contract Profile {
     struct UserProfile {
         string userName;
         string bio;
+        string joinedDate;
     }
 
     mapping ( address => UserProfile ) public profiles;
 
-    function setProfile ( string memory userName, string memory bio ) public {
-        profiles[msg.sender] = UserProfile( userName, bio ) ;
+    function setProfile ( string memory userName, string memory bio, string memory joinedDate ) public {
+        profiles[msg.sender] = UserProfile( userName, bio, joinedDate ) ;
     }
 
     function getProfile ( address userAddress) public view returns (UserProfile memory) {
